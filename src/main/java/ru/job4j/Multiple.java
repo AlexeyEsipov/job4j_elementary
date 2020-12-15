217733640ae99d42443de0154b76ed34013198f6
AlexeyEsipov <a_esipov@list.ru>
2020-12-15 12:57:03 +0300

    0.1. FileOutputStream. [#379927]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,17 +1,23 @@
 package ru.job4j;
 
+import java.io.FileOutputStream;
+
 public class Multiple {
 
     public static void main(String[] args) {
-        System.out.println("1 * 1 = 1");
-        System.out.println("1 * 2 = 2");
-        System.out.println("1 * 3 = 3");
-        System.out.println("1 * 4 = 4");
-        System.out.println("1 * 5 = 5");
-        System.out.println("1 * 6 = 6");
-        System.out.println("1 * 7 = 7");
-        System.out.println("1 * 8 = 8");
-        System.out.println("1 * 9 = 9");
-        System.out.println("1 * 10 = 10");
+        try (FileOutputStream out = new FileOutputStream("multiple.txt")) {
+            out.write("1 * 1 = 1".getBytes());
+            out.write("1 * 2 = 2".getBytes());
+            out.write("1 * 3 = 3".getBytes());
+            out.write("1 * 4 = 4".getBytes());
+            out.write("1 * 5 = 5".getBytes());
+            out.write("1 * 6 = 6".getBytes());
+            out.write("1 * 7 = 7".getBytes());
+            out.write("1 * 8 = 8".getBytes());
+            out.write("1 * 9 = 9".getBytes());
+            out.write("1 * 10 = 10".getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
