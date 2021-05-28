e0c2260f01fed7157238f1bcf46b77692c126716
AlexeyEsipov <a_esipov@list.ru>
2021-05-28 15:22:39 +0300

    6.0.1.1. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -4,9 +4,13 @@ public class ArrayDefinition {
 
     public static void main(String[] args) {
         short[] ages = new short[10];
+        System.out.println("Размер массива ages[] равен: " + ages.length);
         String[] surnames = new String[100500];
+        System.out.println("Размер массива surnames[] равен: " + surnames.length);
         float[] prices = new float[40];
+        System.out.println("Размер массива prices[] равен: " + prices.length);
         String[] names = new String[4];
+        System.out.println("Размер массива names[] равен: " + names.length);
         names[0] = "Petrov";
         names[1] = "Sidorov";
         names[2] = "Ivanov";
